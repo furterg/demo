1ceefa8de594284228b1efb8895adc0a97b5e939
Greg <gfurter@protonmail.ch>
2019-04-25 23:07:56 +0200

    Splitting cover.sh into several line, 1 line per command

--- cover.sh
+++ cover.sh
@@ -1,4 +1,9 @@
 #!/bin/bash
 
 # The basic command line I use and want to turn into a script:
-alias cover='mkdir cover ; mv *_1.pdf ./cover ; rm sheet* ; mv ./cover/* . ; rmdir cover/ ; rename -e 's/_1//' sheet*.pdf'
+mkdir cover
+mv *_1.pdf ./cover
+rm sheet*
+mv ./cover/* .
+rmdir cover/
+rename -e 's/_1//' sheet*.pdf'
